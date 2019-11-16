7a6fe39db50458c3bfb34e1540432625ced53574
Matheus <mathjvmf@gmail.com>
2019-11-16 12:30:59 -0300

    Para o tooltip funcionar, MyButton deve estar sob um Widget. Mas fazer o posicionamento disso está complicado.

--- src/main.py
+++ src/main.py
@@ -12,6 +12,7 @@ Config.set('kivy','window_icon','img.png')
 from kivy.core.window import Window
 from kivy.uix.actionbar import ActionButton
 from kivy.clock import Clock
+from kivy.uix.widget import Widget
 
 from inp import parseInputAlphabet, parseInputPattern
 import os
@@ -124,28 +125,29 @@ class ResultsWindow(Screen):
 class Tooltip(Label):
     pass
 
-class MyButton(ActionButton):
-    tooltip = Tooltip(text = 'Hello world')
+class MyButton(ActionButton, Widget):
+    # tooltip = Tooltip(text = 'Hello world')
 
-    def __init__(self, **kwargs):
-        Window.bind(mouse_pos=self.on_mouse_pos)
-        super(ActionButton, self).__init__(**kwargs)
+    # def __init__(self, **kwargs):
+    #     Window.bind(mouse_pos=self.on_mouse_pos)
+    #     super(ActionButton, self).__init__(**kwargs)
 
-    def on_mouse_pos(self, *args):
-        if not self.get_root_window():
-            return
-        pos = args[1]
-        self.tooltip.pos = pos
-        Clock.unschedule(self.display_tooltip) # cancel scheduled event since I moved the cursor
-        self.close_tooltip() # close if it's opened
-        if self.collide_point(*self.to_widget(*pos)):
-            Clock.schedule_once(self.display_tooltip, 1)
+    # def on_mouse_pos(self, *args):
+    #     if not self.get_root_window():
+    #         return
+    #     pos = args[1]
+    #     self.tooltip.pos = pos
+    #     Clock.unschedule(self.display_tooltip) # cancel scheduled event since I moved the cursor
+    #     self.close_tooltip() # close if it's opened
+    #     if self.collide_point(*self.to_widget(*pos)):
+    #         Clock.schedule_once(self.display_tooltip, 1)
 
-    def close_tooltip(self, *args):
-        Window.remove_widget(self.tooltip)
+    # def close_tooltip(self, *args):
+    #     Window.remove_widget(self.tooltip)
 
-    def display_tooltip(self, *args):
-        Window.add_widget(self.tooltip)
+    # def display_tooltip(self, *args):
+    #     Window.add_widget(self.tooltip)
+    pass
 
 class WindowManager(ScreenManager):
     pass
