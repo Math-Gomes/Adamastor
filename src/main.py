f9bda99e7a6914ed4517935c89c6111ac91726e2
Matheus <mathjvmf@gmail.com>
2019-11-16 11:41:14 -0300

    Mudança da cor dos botoes, mudança do icone da pasta e tentativa de fazer ToolTip

--- src/main.py
+++ src/main.py
@@ -9,14 +9,16 @@ from kivy.config import Config
 Config.set('graphics','resizable', 0)
 Config.set('kivy','window_icon','img.png')
 
+from kivy.core.window import Window
+from kivy.uix.actionbar import ActionButton
+from kivy.clock import Clock
+
 from inp import parseInputAlphabet, parseInputPattern
 import os
 import kmp
 
 from re import findall
 # from kmp_sage import *
-
-from kivy.uix.textinput import TextInput
 
 def showPopup(title, message):
     pop = Popup(title = title, content = Label(text = message), size_hint = (None, None), size = (400, 200))
@@ -44,12 +46,16 @@ class FileWindow(Screen):
         if(filename == []):
             showPopup("Error","No file selected")
             return
-        with open(os.path.join(path, filename[0])) as f:
-            splitInput = findall('\{.*?\}', f.read())
-            if(len(splitInput) != 2):
-                showPopup("Error","Incorrect format")
-                return
-            InputWindow.file_content = splitInput
+        try:
+            with open(os.path.join(path, filename[0])) as f:
+                splitInput = findall('\{.*?\}', f.read())
+                if(len(splitInput) != 2):
+                    showPopup("Error","Incorrect input syntax")
+                    return
+                InputWindow.file_content = splitInput
+        except:
+            showPopup("Error","There was an error reading the file")
+            return
         sm.current = "input"
 
     def select(self, *args): 
@@ -88,7 +94,6 @@ class InputWindow(Screen):
             # eqs, symbs, z = equations_sage(kmp_t)
             # mean = mean_sage(eqs, symbs, z, kmp_t.alph_size)
 
-            # print(type(ResultsWindow.alphabet))
             ResultsWindow.alphabet = self.alphabet.text
             ResultsWindow.pattern = self.pattern.text
             ResultsWindow.mean = str(mean)
@@ -116,6 +121,31 @@ class ResultsWindow(Screen):
         self.pattern_id.text = "Pattern:  " + self.pattern.replace(" ", "")
         self.mean_id.text = "Mean:  " + self.mean
 
+class Tooltip(Label):
+    pass
+
+class MyButton(ActionButton):
+    tooltip = Tooltip(text = 'Hello world')
+
+    def __init__(self, **kwargs):
+        Window.bind(mouse_pos=self.on_mouse_pos)
+        super(ActionButton, self).__init__(**kwargs)
+
+    def on_mouse_pos(self, *args):
+        if not self.get_root_window():
+            return
+        pos = args[1]
+        self.tooltip.pos = pos
+        Clock.unschedule(self.display_tooltip) # cancel scheduled event since I moved the cursor
+        self.close_tooltip() # close if it's opened
+        if self.collide_point(*self.to_widget(*pos)):
+            Clock.schedule_once(self.display_tooltip, 1)
+
+    def close_tooltip(self, *args):
+        Window.remove_widget(self.tooltip)
+
+    def display_tooltip(self, *args):
+        Window.add_widget(self.tooltip)
 
 class WindowManager(ScreenManager):
     pass
