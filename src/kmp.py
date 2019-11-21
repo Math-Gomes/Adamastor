008871fb365c165e5a7f0f35daf337a255521f9c
Math-Gomes <mathjvmf@gmail.com>
2019-11-21 17:30:30 -0300

    suporte a probabilidades customizadas

--- src/kmp.py
+++ src/kmp.py
@@ -3,13 +3,14 @@ import sympy as sym
 import numpy as np # A matriz ajudará nas equações
 import string # Possui o alfabeto para ser usado como entrada
 
-KMP = namedtuple('KMP', 'fsm pat_size alph_size')
+KMP = namedtuple('KMP', 'fsm pat_size alph_size pat')
 
 def kmp(pat, alphabet=None):
-    if not alphabet:
-        alphabet = list(set(pat))
-    else:
-        alphabet = list(alphabet)
+    alphabet = list(alphabet or set(pat))
+    # if not alphabet:
+    #     alphabet = list(set(pat))
+    # else:
+    #     alphabet = list(alphabet)
     m = len(pat)
 
     fsm = {c:[0]*m for c in alphabet}
@@ -22,7 +23,7 @@ def kmp(pat, alphabet=None):
         fsm[pat[j]][j] = j+1
         x = fsm[pat[j]][x]
 
-    return KMP(fsm, m, len(alphabet))
+    return KMP(fsm, m, len(alphabet), pat)
 
 
 def search(txt, pat):
@@ -60,7 +61,41 @@ def equations(kmp_t):
     for i,s in enumerate(symbs[:-1]):
         eq = sym.Eq(s, eqr[i])
         eqs.append(eq)
+    
     eq = sym.Eq(symbs[-1], z*symbs[-2])
+    eqs.append(eq)
+
+    return eqs, z
+
+def equations_(kmp_t, prob_dict):
+    fsm, m, n,_ = kmp_t
+    z = sym.symbols('z')
+    mat = np.array(list(fsm.values()))
+    fsm_keys = list(fsm.keys())
+
+    #Cria as variáveis simbólicas para cada estado do autômato
+    symbs = [sym.symbols('S_'+str(i)) for i in range(m+1)]
+##    for i in range(m+1):
+##        sym_name = sym.symbols('S_'+str(i))
+##        symbs.append(sym_name)
+
+    eqr = [0]*m
+    eqr[0] = 1
+    # for j,k in enumerate(mat.T):
+    #     counts = np.bincount(k, minlength=m+1)
+    #     for i in range(m):
+    #         eqr[i] += counts[i]*z*symbs[j]
+    for i,_ in enumerate(symbs[:-1]):
+        pos = np.where(mat == i)
+        for row,col in zip(*pos):
+            eqr[i] += prob_dict[fsm_keys[row]]*z*symbs[col]
+
+    eqs = []
+    for i,s in enumerate(symbs[:-1]):
+        eq = sym.Eq(s, eqr[i])
+        eqs.append(eq)
+    ll = kmp_t.pat[-1]
+    eq = sym.Eq(symbs[-1],  prob_dict[ll]*z*symbs[-2])
     eqs.append(eq)
 
     return eqs, z
@@ -75,6 +110,14 @@ def mean(eqs, z, alph_size=2):
 
     return g_.subs(z, 1)
 
+def mean_(eqs, z):
+    aux,_ = eqs[-1].args
+    g = sym.solve(eqs, exclude=[z])
+    g = g[aux]
+
+    g_ = sym.diff(g, z)
+
+    return g_.subs(z, 1)
 
 
 # sym.init_printing()
@@ -98,3 +141,17 @@ def mean(eqs, z, alph_size=2):
 # #     print(j, '=', k, ', ', end='')
 
 # print('Média:',mean(eqs,z, kmp_t.alph_size))
+
+if __name__ == "__main__":
+    pat = 'kkkk'
+    alph = 'ck'
+    prob = {
+        'c':.01,
+        'k':.99,
+    }
+    kmp_t = kmp(pat, alph)
+    eqs, z = equations_(kmp_t, prob)
+    for i in eqs:
+        s = sym.pretty(i)
+        print(s)
+    print('Média:', mean_(eqs,z))
